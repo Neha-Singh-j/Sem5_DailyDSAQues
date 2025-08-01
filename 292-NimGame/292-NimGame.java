6058fc33cec1b8efd1692eada89f81bb625e70be
Neha Singh <165409678+Neha-Singh-j@users.noreply.github.com>
2025-08-01 22:37:55 +0530

    Create 292-NimGame

---
+++ 292-NimGame/292-NimGame.java
@@ -0,0 +1,11 @@
+// Last updated: 8/1/2025, 10:37:54 PM
+class Solution {
+    public boolean canWinNim(int n) {
+        
+        if (n % 4 != 0) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+}
