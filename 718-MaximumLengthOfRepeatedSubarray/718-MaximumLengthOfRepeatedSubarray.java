83b5c3f440e0d8ae56204a40992e2f358c79f3a5
Neha Singh <165409678+Neha-Singh-j@users.noreply.github.com>
2025-08-01 22:35:51 +0530

    Create 718-MaximumLengthOfRepeatedSubarray

---
+++ 718-MaximumLengthOfRepeatedSubarray/718-MaximumLengthOfRepeatedSubarray.java
@@ -0,0 +1,27 @@
+// Last updated: 8/1/2025, 10:35:50 PM
+class Solution {
+    public int findLength(int[] nums1, int[] nums2) {
+        int p1 = 0;
+        int res = 0;
+
+        while (p1 < nums1.length) {
+            int p2 = 0;
+            while (p2 < nums2.length) {
+                int c = 0;
+                int i = p1, j = p2;
+
+                while (i < nums1.length && j < nums2.length && nums1[i] == nums2[j]) {
+                    c++;
+                    i++;
+                    j++;
+                }
+
+                res = Math.max(c, res);
+                p2++;
+            }
+            p1++;
+        }
+
+        return res;
+    }
+}
