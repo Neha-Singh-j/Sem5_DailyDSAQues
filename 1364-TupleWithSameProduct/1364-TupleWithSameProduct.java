34a8125a511a96b2a9680638775c59bcd237bf32
Neha Singh <165409678+Neha-Singh-j@users.noreply.github.com>
2025-08-01 22:33:57 +0530

    Create 1364-TupleWithSameProduct

---
+++ 1364-TupleWithSameProduct/1364-TupleWithSameProduct.java
@@ -0,0 +1,44 @@
+// Last updated: 8/1/2025, 10:33:56 PM
+import java.util.*;
+
+class Solution {
+    public int tupleSameProduct(int[] nums) {
+        int c = 0;
+        
+        ArrayList<Integer> map = new ArrayList<>();
+        ArrayList<Integer> key = new ArrayList<>();
+        
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = i + 1; j < nums.length; j++) {
+                map.add(nums[i] * nums[j]);
+            }
+        }
+        
+        Collections.sort(map); // Sorting was missing
+        
+        ArrayList<Integer> result = new ArrayList<>();
+        int j = 0;
+
+        while (j < map.size()) {
+            int count = 1;
+            int val = map.get(j);
+            j++;
+
+            while (j < map.size() && map.get(j).equals(val)) { // Fixed comparison issue
+                count++;
+                j++;
+            }
+
+            result.add(val);
+            result.add(count);
+        }
+
+        // Calculate final count
+        for (int i = 1; i < result.size(); i += 2) { // Iterate directly on frequency values
+            int n = result.get(i);
+            c += 8 * n * (n - 1) / 2;
+        }
+      
+        return c;
+    }
+}
