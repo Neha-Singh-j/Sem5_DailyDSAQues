15d3d1f26ed224d6e5f12d9b5bb4e3ff389d94d9
Neha Singh <165409678+Neha-Singh-j@users.noreply.github.com>
2025-08-01 22:34:25 +0530

    Create 1078-RemoveOutermostParentheses

---
+++ 1078-RemoveOutermostParentheses/1078-RemoveOutermostParentheses.java
@@ -0,0 +1,25 @@
+// Last updated: 8/1/2025, 10:34:24 PM
+class Solution {
+    public String removeOuterParentheses(String s) {
+        Stack<Character> st = new Stack<>();
+        StringBuilder sb = new StringBuilder();
+
+        for (int i = 0; i < s.length(); i++) {
+            char ch = s.charAt(i);
+
+            if (ch == '(') {
+                if (!st.isEmpty()) {
+                    sb.append(ch); // not outermost
+                }
+                st.push(ch);
+            } else if (ch == ')') {
+                st.pop();
+                if (!st.isEmpty()) {
+                    sb.append(ch); // not outermost
+                }
+            }
+        }
+
+        return sb.toString();
+    }
+}
