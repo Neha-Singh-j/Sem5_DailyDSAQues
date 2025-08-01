20e73d6c5c461116aca02f0babc9e1226f9fe8bc
Neha Singh <165409678+Neha-Singh-j@users.noreply.github.com>
2025-08-01 22:40:45 +0530

    Create 83-RemoveDuplicatesFromSortedList

---
+++ 83-RemoveDuplicatesFromSortedList/83-RemoveDuplicatesFromSortedList.java
@@ -0,0 +1,27 @@
+// Last updated: 8/1/2025, 10:40:44 PM
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode deleteDuplicates(ListNode head) {
+        ListNode curr=head;
+        if(curr==null || curr.next==null){
+            return head;
+        }
+        while(curr.next!=null){
+            if(curr.val==curr.next.val){
+                curr.next=curr.next.next;
+            }else{
+                curr=curr.next;
+            }
+        }
+        return head;
+    }
+}
