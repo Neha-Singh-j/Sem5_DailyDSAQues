19e5c24c9322365cca3bccacfb65275081e14910
Neha Singh <165409678+Neha-Singh-j@users.noreply.github.com>
2025-08-20 22:59:52 +0530

    Create 1457-Pseudo-PalindromicPathsinaBinaryTree

---
+++ 1457-Pseudo-PalindromicPathsinaBinaryTree/1457-Pseudo-PalindromicPathsinaBinaryTree.java
@@ -0,0 +1,51 @@
+// Last updated: 8/20/2025, 10:59:48 PM
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public int pseudoPalindromicPaths (TreeNode root) {
+        int[] fr =new int[10];
+       return count(root,fr);
+    }
+    public static int count(TreeNode root, int[] fr ){
+        if(root==null){
+            return 0;
+        }
+
+        int c=0;
+        fr[root.val]++;
+        if(root.left==null && root.right==null){
+            if(palindrome(fr)){
+                c=1;
+            }
+        }else{
+            c+=count(root.left,fr);
+            c+=count(root.right,fr);
+         }
+        fr[root.val]--;
+        return c;
+
+    }
+    public static boolean palindrome(int[] fr){
+        int odd=0;
+        for(int i=1;i<10;i++){
+            if(fr[i]%2!=0){
+                odd++;
+            }
+            if(odd>1) return false;
+        }
+        return true;
+    }
+}
