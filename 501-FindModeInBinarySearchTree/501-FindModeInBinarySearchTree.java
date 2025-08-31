ac148ca5a8363afdd6385013db71b045f50fbfac
Neha Singh <165409678+Neha-Singh-j@users.noreply.github.com>
2025-08-31 16:16:25 +0530

    Create 501-FindModeInBinarySearchTree

---
+++ 501-FindModeInBinarySearchTree/501-FindModeInBinarySearchTree.java
@@ -0,0 +1,49 @@
+// Last updated: 8/31/2025, 4:16:24 PM
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+
+class Solution {
+    public int[] findMode(TreeNode root) {
+        Map<Integer, Integer> map = new HashMap<>();
+        
+        traverse(root, map);
+        
+        int maxFreq = 0;
+        for (int freq : map.values()) {
+            maxFreq = Math.max(maxFreq, freq);
+        }
+        
+        List<Integer> modes = new ArrayList<>();
+        for (Map.Entry<Integer, Integer> entry : map.entrySet()) {
+            if (entry.getValue() == maxFreq) {
+                modes.add(entry.getKey());
+            }
+        }
+        
+        int[] result = new int[modes.size()];
+        for (int i = 0; i < modes.size(); i++) {
+            result[i] = modes.get(i);
+        }
+        return result;
+    }
+    
+    private void traverse(TreeNode node, Map<Integer, Integer> map) {
+        if (node == null) return;
+        map.put(node.val, map.getOrDefault(node.val, 0) + 1);
+        traverse(node.left, map);
+        traverse(node.right, map);
+    }
+}
