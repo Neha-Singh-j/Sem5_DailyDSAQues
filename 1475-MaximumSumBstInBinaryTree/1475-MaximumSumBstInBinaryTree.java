654c91cc84702bf3ad549a92f31004b58d137f40
Neha Singh <165409678+Neha-Singh-j@users.noreply.github.com>
2025-08-31 16:14:33 +0530

    Create 1475-MaximumSumBstInBinaryTree

---
+++ 1475-MaximumSumBstInBinaryTree/1475-MaximumSumBstInBinaryTree.java
@@ -0,0 +1,47 @@
+// Last updated: 8/31/2025, 4:14:32 PM
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public int maxSumBST(TreeNode root) {
+        return ValidBST(root).ans;
+    }
+    public BstPair ValidBST(TreeNode root){
+		    	if(root==null) return new BstPair();
+		    	BstPair lbp=ValidBST(root.left);
+		    	BstPair rbp=ValidBST(root.right);
+		    	BstPair sbp=new BstPair();
+		    	sbp.min=Math.min(lbp.min,Math.min(rbp.min,root.val));
+		    	sbp.sum=lbp.sum+rbp.sum+root.val;
+		    	sbp.max=Math.max(lbp.max,Math.max(rbp.max, root.val));
+                sbp.isbst=lbp.isbst && rbp.isbst && lbp.max < root.val && rbp.min > root.val;           if(sbp.isbst){
+                    // when current root is bst
+                    sbp.ans=Math.max(lbp.ans,Math.max(rbp.ans,sbp.sum));
+                }else{
+                    // when im not bst ehn from my left and right which is max is answer
+                    sbp.ans=Math.max(lbp.ans,rbp.ans);
+                }
+                
+		    	return sbp;
+		    }
+
+		    class BstPair{
+		    	boolean isbst=true;
+		    	Long max=Long.MIN_VALUE;
+		    	Long min=Long.MAX_VALUE;
+                int sum=0;
+                int ans=0;
+		    }
+}
