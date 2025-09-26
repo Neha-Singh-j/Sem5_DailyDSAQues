3d8f0454cee698b4de6d748455d24f3f4b8e9bb0
Neha Singh <165409678+Neha-Singh-j@users.noreply.github.com>
2025-09-26 21:11:46 +0530

    File updated at 9/26/2025, 9:11:45 PM

--- 34-FindFirstandLastPositionofElementinSortedArray/34-FindFirstandLastPositionofElementinSortedArray.java
+++ 34-FindFirstandLastPositionofElementinSortedArray/34-FindFirstandLastPositionofElementinSortedArray.java
@@ -1,37 +1,33 @@
-// Last updated: 9/10/2025, 12:45:49 AM
+// Last updated: 9/26/2025, 9:11:45 PM
 class Solution {
     public int[] searchRange(int[] nums, int target) {
-        int[] result = {-1, -1};
-        int left = binarySearch(nums, target, true);
-        int right = binarySearch(nums, target, false);
-        result[0] = left;
-        result[1] = right;
-        return result;        
+        int[] arr={-1,-1};
+        int l=binarySearch(nums,target,true);
+        int r=binarySearch(nums,target,false);
+        arr[0]=l;
+        arr[1]=r;
+        return arr;
     }
-
-    private int binarySearch(int[] nums, int target, boolean isSearchingLeft) {
-        int left = 0;
-        int right = nums.length - 1;
-        int idx = -1;
-
-        while (left <= right) {
-            int mid = left + (right - left) / 2;
-            
-            if (nums[mid] > target) {
-                right = mid - 1;
-            } else if (nums[mid] < target) {
-                left = mid + 1;
-            } else {
-                idx = mid;
-                if (isSearchingLeft) {
-                    right = mid - 1;
-                } else {
-                    left = mid + 1;
+    public static int binarySearch(int[] nums, int target, boolean searchLeft){
+        int l=0;
+        int r=nums.length-1;
+        int i=-1;
+        while(l<=r){
+            int mid=l+(r-l)/2;
+            if(nums[mid]>target){
+                r=mid-1;
+            }else if(nums[mid]<target){
+                l=mid+1;
+            }
+            else{
+                i=mid;
+                if(searchLeft){
+                    r=mid-1;
+                }else{
+                    l=mid+1;
                 }
             }
         }
-
-        return idx;
+        return i;
     }
-
 }
