8a84b96777cc2379b27314fc6fa30063831c5a99
Neha Singh <165409678+Neha-Singh-j@users.noreply.github.com>
2025-10-21 21:10:00 +0530

    Time: 7 ms (41.26%) | Memory: 45.3 MB (86.93%) - LeetSync

---
+++ 735-asteroid-collision/asteroid-collision.java
@@ -0,0 +1,33 @@
+class Solution {
+    public int[] asteroidCollision(int[] asteroids) {
+        Stack<Integer> st=new Stack<>();
+        for(int a:asteroids){
+             boolean des = false;
+            while(!st.isEmpty() && st.peek() > 0 && a < 0){
+                if(Math.abs(a)>Math.abs(st.peek())){
+                    st.pop();
+                }else if(Math.abs(a)==Math.abs(st.peek())){
+                    st.pop();
+                    des=true;
+                    break;
+                }else{
+                    des=true;
+                    break;
+                }
+            }
+            if (!des) {
+                st.push(a); // current asteroid survived
+            }
+        }
+        int[] res = new int[st.size()];
+        for (int i = st.size() - 1; i >= 0; i--) {
+            res[i] = st.pop();
+        }
+        return res;
+    }
+}
+
+//3 conditions are there 
+    //1.current is smaller than top of stack
+    //2.current is equal top of stack
+    //3.current is bigger than top of stack
