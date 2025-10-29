e746b1498d2127e6707964f8e6b2c6c75dccc727
Neha Singh <165409678+Neha-Singh-j@users.noreply.github.com>
2025-10-29 09:18:12 +0530

    Create 3713-LongestBalancedSubstringI

---
+++ 3713-LongestBalancedSubstringI/3713-LongestBalancedSubstringI.java
@@ -0,0 +1,28 @@
+// Last updated: 10/29/2025, 9:18:11 AM
+class Solution {
+    public int longestBalanced(String s) {
+        int n = s.length();
+        int len = 0;
+        for (int i = 0; i < n; i++) {
+            int[] freq = new int[26]; 
+            for (int j = i; j < n; j++) {
+                freq[s.charAt(j) - 'a']++; 
+                if (isBalanced(freq)) {
+                    len = Math.max(len, j - i + 1);
+                }
+            }
+        }
+        return len;
+    }
+
+    private boolean isBalanced(int[] freq) {
+        int c = -1;
+        for (int f : freq) {
+            if (f > 0) {
+                if (c == -1) c = f;
+                else if (f != c) return false;
+            }
+        }
+        return true;
+    }
+}
