4084bda138ac35780481337136b9859d1b9dbbb8
Neha Singh <165409678+Neha-Singh-j@users.noreply.github.com>
2025-11-03 18:22:55 +0530

    File updated at 11/3/2025, 6:22:55 PM

--- 2-AddTwoNumbers/2-AddTwoNumbers.java
+++ 2-AddTwoNumbers/2-AddTwoNumbers.java
@@ -1,4 +1,4 @@
-// Last updated: 8/1/2025, 10:43:09 PM
+// Last updated: 11/3/2025, 6:22:55 PM
 /**
  * Definition for singly-linked list.
  * public class ListNode {
@@ -9,42 +9,34 @@
  *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
  * }
  */
-
 class Solution {
     public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
-        ListNode dummyHead = new ListNode(0);
-        ListNode current = dummyHead;
-        int carry = 0;
-        
-        while (l1 != null || l2 != null) {
-            // int val1 = (l1 != null) ? l1.val : 0;
-            // int val2 = (l2 != null) ? l2.val : 0;
-            int val1,val2;
-            if(l1!=null){
-                val1=l1.val;
-            }else{
-                val1=0;
-            }
-            if(l2!=null){
-                val2=l2.val;
-            }else{
-                val2=0;
-            }
-            int sum = val1 + val2 + carry;
-            
-            carry = sum / 10;
-            int res = sum % 10;
-            current.next = new ListNode(res);
-            current = current.next;
-            
-            if (l1 != null) l1 = l1.next;
-            if (l2 != null) l2 = l2.next;
+        int carry=0;
+        ListNode res=new ListNode();
+        ListNode dummy=res;
+        while(l1!=null && l2 !=null){
+            dummy.next=new ListNode((l1.val+l2.val+carry)%10);
+            carry=(l1.val+l2.val+carry)/10;
+            dummy=dummy.next;
+            l1=l1.next;
+            l2=l2.next;
         }
-        
-        if (carry > 0) {
-            current.next = new ListNode(carry);
+        while(l1!=null){
+             dummy.next=new ListNode((l1.val+carry)%10);
+            carry=(l1.val+carry)/10;
+            dummy=dummy.next;
+            l1=l1.next;
         }
-        
-        return dummyHead.next;
+         while(l2!=null){
+             dummy.next=new ListNode((l2.val+carry)%10);
+            carry=(l2.val+carry)/10;
+            dummy=dummy.next;
+            l2=l2.next;
+        }
+        if(carry!=0){
+            dummy.next=new ListNode(carry);
+            dummy=dummy.next;
+        }
+        return res.next;
     }
 }
