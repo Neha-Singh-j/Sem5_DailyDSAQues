f7da1033c7a64b2c0f5179c8015265f1e8e7c71e
Neha Singh <165409678+Neha-Singh-j@users.noreply.github.com>
2025-11-06 18:21:49 +0530

    Time: 11 ms (11.55%) | Memory: 47.1 MB (5.70%) - LeetSync

---
+++ 207-course-schedule/course-schedule.java
@@ -0,0 +1,52 @@
+class Solution {
+    private HashMap<Integer, List<Integer>> map;
+    public boolean canFinish(int numCourses, int[][] prerequisites) {
+      
+        map=new HashMap<>();
+		for(int i=0;i<numCourses;i++) {
+			map.put(i, new ArrayList<>());
+		}
+
+       for(int i=0;i<prerequisites.length;i++){
+             AddEdge(prerequisites[i][0],prerequisites[i][1]);
+       }
+        return !isCycle();
+    }
+  
+	
+	public void AddEdge(int v1,int v2) {
+		map.get(v1).add(v2);
+	}
+	
+	public int[] indegree(){
+		int[] in=new int[map.size()];
+		for(int v1:map.keySet()) {
+			for(int v2:map.get(v1)) {
+				in[v2]++;
+			}
+		}
+		return in;
+	}
+	 public boolean isCycle() {
+		 
+		 Queue<Integer> q=new LinkedList<>();
+		 int[] in=indegree();
+		 for(int i=0;i<in.length;i++) {
+			 if(in[i]==0) {
+				 q.add(i);
+			 }
+		 }
+		 int c=0;
+		 while(!q.isEmpty()) {
+			 int r= q.poll();
+			c++;
+			 for(int nbrs: map.get(r)) {
+				 in[nbrs]--;
+				 if(in[nbrs]==0) {
+					 q.add(nbrs);
+				 }
+			 }
+		 }
+		 return c!=map.size();
+     }
+}
