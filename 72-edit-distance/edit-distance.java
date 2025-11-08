c37423662e3992a2987fa800fa11a8b6ad3f8c9a
Neha Singh <165409678+Neha-Singh-j@users.noreply.github.com>
2025-11-09 01:11:33 +0530

    Time: 3 ms (95.24%) | Memory: 46.6 MB (10.58%) - LeetSync

---
+++ 72-edit-distance/edit-distance.java
@@ -0,0 +1,31 @@
+class Solution {
+    public int minDistance(String word1, String word2) {
+        int[][] dp = new int[word1.length()][word2.length()];
+        for (int[] row : dp) {
+            Arrays.fill(row, -1);
+        }
+        return Min_Ops(word1, word2, 0, 0, dp);
+    }
+
+    public static int Min_Ops(String s, String t, int i, int j, int[][]dp) {
+		
+		if(i==s.length()) {
+			return t.length()-j;
+		}
+		if(j==t.length()) {
+			return s.length()-i;
+		}
+        if(dp[i][j]!=-1) return dp[i][j];
+		int ans=0;
+		if(s.charAt(i)==t.charAt(j)) {
+			ans=Min_Ops(s,t,i+1,j+1,dp);
+		}else {
+			int D=Min_Ops(s,t,i+1,j,dp);
+			int R=Min_Ops(s,t,i+1,j+1,dp);
+			int I=Min_Ops(s,t,i,j+1,dp);
+			ans=1+Math.min(D, Math.min(R,I));
+		}
+         return dp[i][j] = ans;
+    }
+    
+}
