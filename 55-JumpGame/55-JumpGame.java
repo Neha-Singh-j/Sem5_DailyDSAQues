08a0feeeeeabd630afdd4fdced8e0f93e709d726
Neha Singh <165409678+Neha-Singh-j@users.noreply.github.com>
2025-11-12 19:47:16 +0530

    File updated at 11/12/2025, 7:47:14 PM

--- 55-JumpGame/55-JumpGame.java
+++ 55-JumpGame/55-JumpGame.java
@@ -1,34 +1,17 @@
-// Last updated: 9/20/2025, 9:13:51 PM
+// Last updated: 11/12/2025, 7:47:14 PM
 class Solution {
     public boolean canJump(int[] nums) {
-        
-        int n = nums.length;
-        // -1 = unvisited, 0 = false, 1 = true
-        int[] dp = new int[n];
-        for (int i = 0; i < n; i++) {
-            dp[i] = -1;
-        }
-        return helper(0, nums, dp);
-    }
-
-    private boolean helper(int idx, int[] nums, int[] dp) {
-        int n = nums.length;
-        if (idx == n - 1) {
-            return true;
-        }
-
-        if (dp[idx] != -1) {
-            return dp[idx] == 1;
-        }
-        int maxJump = nums[idx];
-        for (int i = 1; i <= maxJump; i++) {
-            int next = idx + i;
-            if (next < n && helper(next, nums, dp)) {
-                dp[idx] = 1; 
+        int n=nums.length;
+        int maxReach=0;
+        for(int i=0;i<n;i++){
+            if(i>maxReach){
+                return false;
+            }
+            maxReach=Math.max(maxReach,i+nums[i]);
+            if(maxReach>=n-1){
                 return true;
             }
         }
-        dp[idx] = 0;
-        return false;
+        return true;
     }
 }
