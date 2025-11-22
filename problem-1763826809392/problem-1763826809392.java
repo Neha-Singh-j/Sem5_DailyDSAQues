f6bc7bb39c4cd6dac8ac68ec491433a8217b089d
Neha Singh <165409678+Neha-Singh-j@users.noreply.github.com>
2025-11-22 21:23:30 +0530

    Create

---
+++ problem-1763826809392/problem-1763826809392.java
@@ -0,0 +1,21 @@
+// Last updated: 11/22/2025, 9:23:29 PM
+class Solution {
+    public int[] lexSmallestNegatedPerm(int n, long target) {
+        long n1=(long) n*(n+1)/2;
+        if ((n1 - target) % 2 != 0) return new int[0];
+        long check = (n1 - target) / 2;
+        if (check < 0) return new int[0];
+        int[] perm=new int[n];
+        for(int i=n;i>=1;i--){
+            if(check>=i){
+                perm[i-1]=-i;
+                check-=i;
+            }else{
+                perm[i-1]=i;
+            }
+        }
+        Arrays.sort(perm);
+        if(check!=0) return new int[0];
+        return perm;
+    }
+}
