3283d973a8110657a2732ca079b9dc89d3e71e5d
Neha Singh <165409678+Neha-Singh-j@users.noreply.github.com>
2025-11-23 09:01:40 +0530

    Create

---
+++ problem-1763868699723/problem-1763868699723.java
@@ -0,0 +1,17 @@
+// Last updated: 11/23/2025, 9:01:39 AM
+class Solution {
+    public long sumAndMultiply(int n) {
+        String st=String.valueOf(n);
+        long curr=0;
+        long sum=0;
+        char[] arr=st.toCharArray();
+        for(char ch: arr){
+            if(ch!='0'){
+                long rem=(ch-'0');
+                curr=curr*10+rem;
+                sum+=rem;
+            }
+        }
+        return sum*curr;
+    }
+}
