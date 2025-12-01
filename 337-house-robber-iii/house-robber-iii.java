15fa0bda4b8a784f940b49819d97e0b9618b9980
Neha Singh <165409678+Neha-Singh-j@users.noreply.github.com>
2025-12-01 22:36:03 +0530

    Time: 2 ms (37.64%) | Memory: 47 MB (7.02%) - LeetSync

---
+++ 337-house-robber-iii/house-robber-iii.java
@@ -0,0 +1,40 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public int rob(TreeNode root) {
+        HashMap<TreeNode, Integer>map=new HashMap<>();
+        return robber(root,map);
+    }
+    public static int robber(TreeNode root,HashMap<TreeNode, Integer> map){
+        if(root==null){
+            return 0;
+        }
+        if(map.containsKey(root)) return map.get(root);
+        int select=root.val;
+        if(root.left!=null){
+            select+=robber(root.left.left,map)+robber(root.left.right,map);
+       
+        }
+         if(root.right!=null){
+            select+=robber(root.right.left,map)+robber(root.right.right,map);
+       
+        }
+         int not_select=robber(root.left,map)+robber(root.right,map);
+         int res=Math.max(select,not_select);
+         map.put(root,res);
+        return res;
+    }
+}
