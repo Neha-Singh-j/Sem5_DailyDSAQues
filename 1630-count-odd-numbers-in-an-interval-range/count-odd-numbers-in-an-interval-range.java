bb5358746188502d30af5fc68d2715c52c2ba6fc
Neha Singh <165409678+Neha-Singh-j@users.noreply.github.com>
2025-12-07 17:11:02 +0530

    Time: 0 ms (100.00%) | Memory: 42.1 MB (21.38%) - LeetSync

---
+++ 1630-count-odd-numbers-in-an-interval-range/count-odd-numbers-in-an-interval-range.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int countOdds(int low, int high) {
+        int c=0;
+        c+=(high-low)/2;
+        if(low%2!=0){
+            c++;
+        }else if(high%2!=0){
+            c++;
+        }
+        return c;
+    }
+}
