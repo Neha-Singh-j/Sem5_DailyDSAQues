7c52c24f3db639b2455f0bb3248b27bc4e4db6fd
Neha Singh <165409678+Neha-Singh-j@users.noreply.github.com>
2026-01-30 13:00:20 +0530

    Time: 45 ms (95.21%) | Memory: 98.7 MB (7.19%) - LeetSync

---
+++ 3963-number-of-perfect-pairs/number-of-perfect-pairs.java
@@ -0,0 +1,24 @@
+class Solution {
+    public long perfectPairs(int[] nums) {
+        int n = nums.length;
+        long c = 0;
+        long ans = 0;
+
+         int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = Math.abs(nums[i]);
+        }
+        Arrays.sort(arr);
+        int j = 0;
+        for (int i = 0; i < n; i++) {
+            // move j until condition satisfied
+            while (arr[i] > 2L * arr[j]) {
+                j++;
+            }
+            ans += (i - j);
+        }
+
+        return ans;
+    }
+}
+
