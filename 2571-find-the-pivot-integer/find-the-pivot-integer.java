957abbeeb78349238983761ef5db3c631f0ed533
Neha Singh <165409678+Neha-Singh-j@users.noreply.github.com>
2026-02-10 22:21:20 +0530

    Time: 1 ms (65.72%) | Memory: 42.3 MB (69.70%) - LeetSync

---
+++ 2571-find-the-pivot-integer/find-the-pivot-integer.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int pivotInteger(int n) {
+        int sum = 0, i = 1;
+
+        // total sum
+        while (i <= n) {
+            sum += i;
+            i++;
+        }
+
+        int curr = 0;
+        i = 1;
+
+        while (i <= n) {
+            curr += i;  // include i first
+
+            if (curr == sum - curr + i) {
+                return i;
+            }
+            i++;
+        }
+        return -1;
+    }
+}
