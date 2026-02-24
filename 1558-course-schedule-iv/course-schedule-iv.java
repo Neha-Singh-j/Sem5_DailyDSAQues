9d3e2f0c1cbc5c7ef6480a55ece48352106596aa
Neha Singh <165409678+Neha-Singh-j@users.noreply.github.com>
2026-02-24 12:04:06 +0530

    Time: 19 ms (75.85%) | Memory: 51 MB (49.72%) - LeetSync

--- 1558-course-schedule-iv/course-schedule-iv.java
+++ 1558-course-schedule-iv/course-schedule-iv.java
@@ -1,45 +1,22 @@
 class Solution {
-    public List<Boolean> checkIfPrerequisite(int n, int[][] prerequisites, int[][] queries) {
-        HashMap<Integer, List<Integer>> map = new HashMap<>();
-
-        for (int i = 0; i < n; i++) {
-            map.put(i, new ArrayList<>());
+    public List<Boolean> checkIfPrerequisite(int numCourses, int[][] prerequisites, int[][] queries) {
+        boolean[][] isPrerequisite = new boolean[numCourses][numCourses];
+        for (int[] prereq : prerequisites) {
+            isPrerequisite[prereq[0]][prereq[1]] = true;
         }
-        for (int[] p : prerequisites) {
-            int u = p[0];
-            int v = p[1];
-            map.get(u).add(v);   // directed edge
-        }
-        boolean[][] reachable = new boolean[n][n];
-        // BFS from every node
-        for (int i = 0; i < n; i++) {
-            bfs(i, map, reachable);
-        }
-
-        List<Boolean> ans = new ArrayList<>();
-        for (int[] q : queries) {
-            ans.add(reachable[q[0]][q[1]]);
-        }
-        return ans;
-    }
-
-    public void bfs(int src, HashMap<Integer, List<Integer>> map, boolean[][] reachable) {
-        Queue<Integer> q = new LinkedList<>();
-        boolean[] vis = new boolean[reachable.length];
-
-        q.add(src);
-        vis[src] = true;
-
-        while (!q.isEmpty()) {
-            int node = q.poll();
-
-            for (int nbr : map.get(node)) {
-                if (!vis[nbr]) {
-                    reachable[src][nbr] = true; // important
-                    vis[nbr] = true;
-                    q.add(nbr);
+        for (int k = 0; k < numCourses; k++) {
+            for (int i = 0; i < numCourses; i++) {
+                for (int j = 0; j < numCourses; j++) {
+                    if (isPrerequisite[i][k] && isPrerequisite[k][j]) {
+                        isPrerequisite[i][j] = true;
+                    }
                 }
             }
         }
+        List<Boolean> result = new ArrayList<>();
+        for (int[] query : queries) {
+            result.add(isPrerequisite[query[0]][query[1]]);
+        }
+        return result;
     }
 }
