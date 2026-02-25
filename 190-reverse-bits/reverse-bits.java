3df54f6f26cc73721940893189fa588291b03704
Neha Singh <165409678+Neha-Singh-j@users.noreply.github.com>
2026-02-25 21:48:49 +0530

    Time: 1 ms (62.58%) | Memory: 42.4 MB (81.40%) - LeetSync

---
+++ 190-reverse-bits/reverse-bits.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int reverseBits(int n) {
+        int result = 0;
+
+        for (int i = 0; i < 32; i++) {
+            int bit = n & 1;       // get last bit
+            result = (result << 1) | bit;  // add to result
+            n = n >> 1;           // shift n
+        }
+
+        return result;
+    }
+}
